d312ec88e9f564b626ba9a5bd3ee68a53f2ea0b9
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-04-28 15:17:25 -0500

    Creation

---
+++ instructions.py
@@ -0,0 +1,9 @@
+############ SET-UP
+## py -3 -m venv pizza_venv
+## pizza_venv/scripts/activate
+## pip install django
+
+############ CREATE THE DJANGO PROJECT
+# Create a new project called learning_log
+### django-admin startproject learning_log .
+# Dot is important!! Creates it with a directory structure.
