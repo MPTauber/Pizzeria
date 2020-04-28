3ba1ecbecb48f3a8dddde7de8f1701ea63307df1
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-04-28 16:18:35 -0500

    mapping url and making view

--- instructions.py
+++ instructions.py
@@ -44,9 +44,83 @@
 ############ DEFINING THE MODEL
 # Use models.py in pizzas to define the data we want to manage in our app
 
+############ ACTIVATING THE MODEL
+# Open settings.py in Pizzeria to add the app we created:
+''' 
+INSTALLED_APPS = [
+    #my_apps
+    'pizzas',
+    etc. 
+'''
+
+############ STORING THE INFORMATION
+### py manage.py makemigrations pizzas
+# This command creates a migration file that instructs the database to store any
+# data associated with any new models.
+### py manage.py migrate
+# This command applies the changes in the migration file previously created.
+
+
+############ CRWATING DJANGO ADMIN SITE
+### py manage.py createsuperuser
+# username: PizzaMonster
+# password: p1zzaislife
+
+############  REGISTER MODEL WITH THE ADMIN SITE
+# To use the Topic model, we have to register it with the admin site.
+# The dot in front of models tells Django to look for models.py in the
+# same directory as admin.py
+# The code admin.site.register() tells Django to manage our model through the admin site.
+### Go to admin.py in pizzas and type code.
+'''
+from .models import Pizza
+
+admin.site.register(Pizza)
+'''
+# To check if it worked run server again and click link:
+### py manage.py runserver
+# Add /admin behind link in browser address
+# http://127.0.0.1:8000/admin
+
+############ MIGRATE AND REGISTER THE TOPPING MODEL
+# In terminal: 
+###py manage.py makemigrations pizzas
+### py manage.py migrate
+# In admin.py
+'''
+from .models import Topping
+
+admin.site.register(Topping)
+'''
+
+############ DJANGO SHELL
+# To examine data programmatically (manually not with server) through an interactive terminal session:
+# Create the Django Shell --> MyShell.py
+# Add code and run. 
+# Can be found in MyShell.py
+
+############ MAPPING THE URL
+# Map the home page URL to something other than the default Django site (With the rocket)
+# Edit the default urls.py file in the Pizzeria folder to look like this:
+'''
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("pizzas.urls")),
+]
+'''
+# This says that we create another pathway in our app (pizzas - not in our project Pizzeria)
+# That command makes it know where to go
+
+# Make a new file, urls.py, in the pizzas folder (the app)
+#### Go into urls.py (in pizzas) to find instructions there.
+
+
+############## FOLLOW POWERPOINTS FOR REST!
 
 #learning_log = Pizzeria
 # learning_logs = pizzas
-
 #Topic = Pizza
 #Entry = Topping
---
+++ pizzas/urls.py
@@ -0,0 +1,27 @@
+# the path function, which is needed when mapping URLs ot views
+from django.urls import path
+
+# the dot tells Python to import the views.py module from
+# the same directory as the current urls.py module.
+from . import views
+
+# The variable app_name helps Django distinguish this urls.py file from
+# files of the same name in other apps within the project.
+app_name = "pizzas"
+
+# The variable urlpatterns in this module is a list of individual pages
+# that can be requested from the learning_logs app.
+urlpatterns = [
+    # the first argument is an empty string ("") which matches the
+    # base URL - http://localhost:88000/. 
+    # The second argument specifies the function name to call in views.py
+    # The third argument provides the name " index" for this URL pattern to refer to it later
+    path("", views.index, name="index"),
+    '''
+    path("topics", views.topics, name="topics"),
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
+    path("new_topic/", views.new_topic, name="new_topic"),
+    ## TEST
+    path("new_entry/<int:topic_id>/", views.new_entry, name="new_entry"),
+    '''
+]
