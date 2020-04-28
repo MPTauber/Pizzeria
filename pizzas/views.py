7556d7131990f2250c0c148bb64d493e592859c8
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-04-28 16:42:01 -0500

    Add pizza_choices

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from .models import Pizza
+
 # Create your views here.
 def index(request):
     """The home page for Pizzeria."""
     return render(request, 'pizzas/index.html')
 
+
+def pizza_choices(request):
+    pizza_choices = Pizza.objects.order_by('name') # alphabetical order
+    context = {'pizza_choices':pizza_choices}
+    return render(request, 'pizzas/pizza_choices.html', context)
